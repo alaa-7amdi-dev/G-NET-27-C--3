826087b210484f4cbc2b1b83ba13bd75a9a6568a
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-04 14:27:20 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@
             //int x = (int)d;
             //Console.WriteLine(x);
             ///// The result is 9: because EXPLICIT CAST was obtained from DOUBLE to INT
+
+            #endregion
+
+            #region Q2
+            int n = 5;
+            double d2 =(double) n / 2;
+            Console.WriteLine(d2);
             #endregion
         }
     }
